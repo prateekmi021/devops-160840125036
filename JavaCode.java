2cc031604ae4fb2c70ad51e6bdcadc4d248d313e
Prateek <prateekmi021@gmail.com>
2022-01-23 18:38:50 +0530

    Solution for question 2

---
+++ JavaCode.java
@@ -0,0 +1,23 @@
+public class JavaCode
+{
+	public static void main(String args[])
+	{
+		add(5,6);
+	}
+	
+	public static void add(int a, int b)
+	{
+		System.out.println("Sum is : "+(a+b));
+	}
+	
+	
+	
+	class InnerClass
+	{
+		int c = 10;
+		public void addInner(int a, int b)
+		{
+			System.out.println("Sum is : "+(c+a+b));
+		}
+	}
+}
