c73a70191bdf94f6237dd7e9b9475e68ef3d84de
Prateek <prateekmi021@gmail.com>
2022-01-23 20:33:16 +0530

    Added Substraction Function

--- JavaCode.java
+++ JavaCode.java
@@ -3,11 +3,17 @@ public class JavaCode
 	public static void main(String args[])
 	{
 		add(5,6);
+		sub(15,6);
 	}
 	
 	public static void add(int a, int b)
 	{
 		System.out.println("Sum is : "+(a+b));
+	}
+	
+	public static void sub(int a, int b)
+	{
+		System.out.println("Sum is : "+(a-b));
 	}
 	
 	
